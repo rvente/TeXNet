aa4343c6e5921fdb87b9c64473a7cf492f369f05
Ralph Vente <venteralph@gmail.com>
2020-05-09 19:55:44 -0400

    final paper started

---
+++ Final-Paper/auto/main.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("babel" "english") ("csquotes" "autostyle") ("biblatex" "backend=biber" "style=alphabetic" "citestyle=authoryear")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "lmodern"
+    "amssymb"
+    "amsmath"
+    "longtable"
+    "booktabs"
+    "comment"
+    "inputenc"
+    "babel"
+    "csquotes"
+    "biblatex")
+   (TeX-add-symbols
+    "KaTeX"
+    "TeXNet")
+   (LaTeX-add-bibliographies))
+ :latex)
+
